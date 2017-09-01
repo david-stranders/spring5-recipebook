e8155b6f35788be0f13dab7b8e2ff65550353538
David Stranders <dstranders@hotmail.com>
2017-09-01 21:46:16 +0200

    Initial commit

---
+++ src/main/java/nl/davidstranders/spring5recipebook/Spring5RecipebookApplication.java
@@ -0,0 +1,12 @@
+package nl.davidstranders.spring5recipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipebookApplication.class, args);
+	}
+}
