48c19a3fe9a81a97a11c68b68bd5093240ac03d1
David Stranders <dstranders@hotmail.com>
2017-09-08 14:14:28 +0200

    Added list and show functionality for Ingredients, inclusive tests

---
+++ src/main/java/nl/davidstranders/spring5recipebook/services/IngredientService.java
@@ -0,0 +1,8 @@
+package nl.davidstranders.spring5recipebook.services;
+
+import nl.davidstranders.spring5recipebook.commands.IngredientCommand;
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
---
+++ src/main/java/nl/davidstranders/spring5recipebook/services/IngredientServiceImpl.java
@@ -0,0 +1,47 @@
+package nl.davidstranders.spring5recipebook.services;
+
+import lombok.extern.slf4j.Slf4j;
+import nl.davidstranders.spring5recipebook.commands.IngredientCommand;
+import nl.davidstranders.spring5recipebook.converters.IngredientToIngredientCommand;
+import nl.davidstranders.spring5recipebook.model.Recipe;
+import nl.davidstranders.spring5recipebook.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class IngredientServiceImpl implements IngredientService {
+
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final RecipeRepository recipeRepository;
+
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if (!recipeOptional.isPresent()){
+            //todo impl error handling
+            log.error("recipe id not found. Id: " + recipeId);
+        }
+
+        Recipe recipe = recipeOptional.get();
+
+        Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
+                .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                .map( ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+
+        if(!ingredientCommandOptional.isPresent()){
+            //todo impl error handling
+            log.error("Ingredient id not found: " + ingredientId);
+        }
+
+        return ingredientCommandOptional.get();
+    }
+}
