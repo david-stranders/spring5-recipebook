489513130114914d0eec48581e46491545c0175b
David Stranders <dstranders@hotmail.com>
2017-09-09 23:54:41 +0200

    Added functionality for saving and displaying images in/out db, including tests

--- src/main/java/nl/davidstranders/spring5recipebook/controllers/IngredientController.java
+++ src/main/java/nl/davidstranders/spring5recipebook/controllers/IngredientController.java
@@ -67,6 +67,7 @@ public class IngredientController {
     @GetMapping("recipe/{recipeId}/ingredient/{id}/update")
     public String updateRecipeIngredient(@PathVariable String recipeId,
                                          @PathVariable String id, Model model){
+
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
 
         model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
---
+++ src/main/java/nl/davidstranders/spring5recipebook/services/ImageService.java
@@ -0,0 +1,8 @@
+package nl.davidstranders.spring5recipebook.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
