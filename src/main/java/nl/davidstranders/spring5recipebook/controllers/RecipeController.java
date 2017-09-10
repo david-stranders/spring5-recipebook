8a9f7564b53cd2f5d2c6c22ef7150396f07d0a05
David Stranders <dstranders@hotmail.com>
2017-09-10 16:13:30 +0200

    Added custom 404 html page

--- src/main/java/nl/davidstranders/spring5recipebook/controllers/RecipeController.java
+++ src/main/java/nl/davidstranders/spring5recipebook/controllers/RecipeController.java
@@ -2,10 +2,13 @@ package nl.davidstranders.spring5recipebook.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import nl.davidstranders.spring5recipebook.commands.RecipeCommand;
+import nl.davidstranders.spring5recipebook.exceptions.NotFoundException;
 import nl.davidstranders.spring5recipebook.services.RecipeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Dell on 6-9-2017.
@@ -56,5 +59,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
