b553a572d5f360c1b029fe0b966fa469b8bac376
David Stranders <dstranders@hotmail.com>
2017-09-13 12:02:43 +0200

    config files for mysql and circleci

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,34 @@
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
+# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+
+# connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+#Create database service accounts - from localhost
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'dstranders';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'dstranders';
+#Create database service accounts - from docker container
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'dstranders';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'dstranders';
+
+#Database grants - localhost
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+
+#Database grants - docker container
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
